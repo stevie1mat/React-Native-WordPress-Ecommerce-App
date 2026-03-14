6931e632620775122ce4b9e4d7b5918237faa132
Steven Mathew <mathewsteven92@gmail.com>
2026-03-14 02:09:28 -0400

    using env

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    WORDPRESS_BASE_URL: 'https://your-wordpress-site.com',
-    WOOCOMMERCE_CONSUMER_KEY: 'ck_your_consumer_key',
-    WOOCOMMERCE_CONSUMER_SECRET: 'cs_your_consumer_secret',
+    WORDPRESS_BASE_URL: process.env.EXPO_PUBLIC_WORDPRESS_URL || 'http://clay-co.local',
+    WOOCOMMERCE_CONSUMER_KEY: process.env.EXPO_PUBLIC_WOO_KEY || 'ck_your_consumer_key',
+    WOOCOMMERCE_CONSUMER_SECRET: process.env.EXPO_PUBLIC_WOO_SECRET || 'cs_your_consumer_secret',
 };
